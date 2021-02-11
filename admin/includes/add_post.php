79ec7b06a00a67c01b4a8d217cdfa157a026d9de
Lightning Farron <kevinnarain94@gmail.com>
2021-02-11 10:07:33 +0100

    Updates in html en php bestanden
    - <select> en <opton> toegevoegd.
    - Notificatie bericht post aanmaken gefixt.
    - Add New button link gewijzigd.
    - Added view post link to admin panel.
    - Username in session weergegeven als profile name.
    - FORM VALIDATIE nog niet goed i.v.m. JS afhandeling.
    - Registratieformulier aangemaakt.
    - If isset op submit registratie + echo.
    - Registratie form input escaped met mysqli_real_escape_string();
    
    Co-Authored-By: OzzyOs <29252324+OzzyOs@users.noreply.github.com>

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -38,6 +38,10 @@ if (isset($_POST['create_post'])) {
     
     $create_post_query = mysqli_query($connection, $query);
     confirmQuery($create_post_query);
+    $the_post_id = mysqli_insert_id($connection);
+    echo "<p class='bg-success'>Post Created. 
+    <a href='../post.php?p_id={$the_post_id}'>View Post</a> or
+    <a href='posts.php'>Edit More Posts</a></p>";
 }
 ?>
 
@@ -71,8 +75,11 @@ if (isset($_POST['create_post'])) {
     </div>
     
     <div class="form-group">
-        <label for="post_status">Post Status</label>
-        <input type="text" class="form-control" name="post_status">
+        <select name="post_status" id="">
+            <option value="draft">Post Status</option>
+            <option value="published">Published</option>
+            <option value="draft">Draft</option>
+        </select>
     </div>
     
     <div class="form-group">
